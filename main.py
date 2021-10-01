104bfd862cb328021939145f228290ca551eff01
Joshua Moore <joshuamoorexyz@protonmail.com>
2021-10-01 12:06:37 -0500

    commendted recieve functionallity until next commit

--- main.py
+++ main.py
@@ -52,36 +52,37 @@ def index():
 
              index()
         if choice==2:
-             mail = imaplib.IMAP4_SSL(gmail_host)
-             mail.login(username,"asdf1AB!")
-             mail.select('INBOX')
+            break
+  #           mail = imaplib.IMAP4_SSL(gmail_host)
+  #           mail.login(username,"asdf1AB!")
+  #           mail.select('INBOX')
              #select specific mails
-             _, selected_mails = mail.search(None, '(FROM "msuwebmail1@gmail.com")')
+  #           _, selected_mails = mail.search(None, '(FROM "msuwebmail1@gmail.com")')
 
 #total number of mails from specific user
-print("Total Messages from msuwebmail1@gmail.com:" , len(selected_mails[0].split()))
+#print("Total Messages from msuwebmail1@gmail.com:" , len(selected_mails[0].split()))
 
-for num in selected_mails[0].split():
-    _, data = mail.fetch(num , '(RFC822)')
-    _, bytes_data = data[0]
+#for num in selected_mails[0].split():
+  #  _, data = mail.fetch(num , '(RFC822)')
+  #  _, bytes_data = data[0]
 
     #convert the byte data to message
-    email_message = email.message_from_bytes(bytes_data)
-    print("\n===========================================")
+  #  email_message = email.message_from_bytes(bytes_data)
+  #  print("\n===========================================")
 
     #access data
-    print("Subject: ",email_message["subject"])
-    print("To:", email_message["to"])
-    print("From: ",email_message["from"])
-    print("Date: ",email_message["date"])
-    for part in email_message.walk():
-        if part.get_content_type()=="text/plain" or part.get_content_type()=="text/html":
-            message = part.get_payload(decode=True)
-            print("Message: \n", message.decode())
-            print("==========================================\n")
-            break
+  #  print("Subject: ",email_message["subject"])
+  #  print("To:", email_message["to"])
+  #  print("From: ",email_message["from"])
+  #  print("Date: ",email_message["date"])
+  #  for part in email_message.walk():
+  #      if part.get_content_type()=="text/plain" or part.get_content_type()=="text/html":
+  #          message = part.get_payload(decode=True)
+  #          print("Message: \n", message.decode())
+  #          print("==========================================\n")
+  #          break
                     #print(str(e))
-            index()
+  #          index()
         if choice==3:
 
             # os.system('cls' if os.name =='nt' else 'clear')
